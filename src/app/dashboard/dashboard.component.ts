b2c73622590b0b4d2f6e340e936338bde6389834
Mariano <macacozzz@hotmail.com>
2024-05-06 15:05:57 -0300

    routes config

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    CommonModule,
+    RouterModule,
   ],
   templateUrl: "./dashboard.component.html",
   styles: `
@@ -15,4 +15,4 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardComponent { }
+export default class DashboardComponent { }
