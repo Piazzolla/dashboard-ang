bd691eb92ea5d58515cc746b5821e53273ee3f85
Mariano <macacozzz@hotmail.com>
2024-05-06 21:52:52 -0300

    dashboard from tailwind components

--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-side-menu',
@@ -7,8 +8,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [
     CommonModule,
   ],
-  template: `<p>side-menu works!</p>`,
-  styleUrl: './side-menu.component.css',
+  templateUrl: "./side-menu.componet.html",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SideMenuComponent { }
+export class SideMenuComponent {
+    public menuItems = routes.map( route => route.children ?? [] )
+    .flat()
+    .filter( route => route && route.path)
+    .filter( route => !route.path!.includes(':'));
+ }
