3243b7f0aac1b4cdec6a5bcb0c996122354b455a
Mariano <macacozzz@hotmail.com>
2024-05-07 14:13:48 -0300

    switch control flow

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
+type Grade = 'A'|'B'|'C'|'F';
+
 @Component({
   selector: 'app-control-flow',
   standalone: true,
@@ -17,9 +19,14 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 })
 export default class ControlFlowComponent {
   public showContent = signal(false);
+  public grade = signal<Grade>('A');
 
   public toggleContent() {
     this.showContent.update( value => !value);
+  }
+
+  public setGrade(newGrade:Grade) {
+    this.grade.update(value => newGrade)
   }
 
 }
